084cd9fdcbf9e32de9dfc3b0bfdbaadba7ff600e
Rene Allen <reneallen75@gmail.com>
2024-08-01 17:01:00 -0600

    added mushroom enemy
    Attempted to add basic enemy behavior

---
+++ Scripts/Enemy - KineBody.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+func _ready():
+	
+	
+	pass
+
+func _physics_process(delta):
+	
+	
+	pass
+	
+func _process(delta):
+	
+	
+	
+	pass
+
+func _on_area_2d_area_entered(area):
+	
+	print(area)
+	pass # Replace with function body.
